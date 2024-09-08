766cac25a9f05b594379b0c93bb77d45a6a2a165
Bryce Monaco <brycedmonaco@gmail.com>
2024-09-08 16:52:25 -0700

    Completed "Jumping and squashing monsters"

--- creeps3d/mob.gd
+++ creeps3d/mob.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal squashed
+
 # Min and max move speeds m/s
 @export var min_speed : int = 10
 @export var max_speed : int = 18
@@ -21,4 +23,8 @@ func _physics_process(delta: float) -> void:
 	
 
 func _on_visible_on_screen_notifier_3d_screen_exited() -> void:
+	queue_free()
+
+func squash() -> void:
+	squashed.emit()
 	queue_free()
--- creeps3d/player.gd
+++ creeps3d/player.gd
@@ -6,6 +6,12 @@ extends CharacterBody3D
 # Downward acceleration in m/s^2
 @export var fall_acceleration : int = 75
 
+# Vertical impulse for jumping in m/s
+@export var jump_impulse : int = 20
+
+# Veritcal impulse for bouncing off of mobs in m/s
+@export var bounce_impulse : int = 16
+
 var target_velocity : Vector3 = Vector3.ZERO
 
 func _physics_process(delta: float) -> void:
@@ -32,4 +38,24 @@ func _physics_process(delta: float) -> void:
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 		
 	velocity = target_velocity
+	
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		target_velocity.y = jump_impulse
+	
+	# Check all collisions in this frame
+	for index in range(get_slide_collision_count()):
+		var collision : KinematicCollision3D = get_slide_collision(index)
+		
+		# Check if the collision is the ground
+		if collision.get_collider() == null:
+			continue
+		elif collision.get_collider().is_in_group("mob"):
+			var mob : Node3D = collision.get_collider()
+			
+			# Check if we are hitting from above
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				break
+	
 	move_and_slide()
